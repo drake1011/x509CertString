f260456aaaa3d6e501d5e76b6433c8d54fb41c2e
Drake Kaempfer <dkaempfer@vcu.com>
2020-01-07 10:06:30 -0600

    Initial commit

---
+++ x509CertString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace x509CertString
+{
+    class Program
+    {
+        static void Main()
+        {
+            do
+            {
+                Console.Write("Enter Cert Path: ");
+                var certPath = Console.ReadLine();
+
+                try
+                {
+                    var cert = X509Certificate.CreateFromCertFile(certPath);
+                    var results = cert.GetPublicKeyString();
+                    Console.WriteLine(results);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            } 
+            while (1 == 1);
+        }
+    }
+}
